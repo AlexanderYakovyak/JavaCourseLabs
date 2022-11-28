4ed1b98863cb1949aa64225451139bfe9c1f6e1e
AlexanderYakovyak <80426990+AlexanderYakovyak@users.noreply.github.com>
2022-11-28 18:07:37 +0200

    Add files via upload

---
+++ Lab12/Task2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Task2 {
+    public static void main(String[] args){
+        Stack<Integer> stk = new Stack<Integer>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, input integer: ");
+        int number = sc.nextInt();
+
+        while(number>1){
+            int part = number%10;
+            stk.push(part);
+            number = number/10;
+        }
+
+        System.out.println("Number of reverse-order digits");
+        for(Integer digit: stk){
+            System.out.print(digit);
+        }
+
+    }
+}
